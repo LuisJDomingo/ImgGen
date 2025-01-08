4393615ca8af9420203c8a1972c73bddb9b9347d
Luis J Domingo <luisdomingogarcia79@gmail.com>
2025-01-08 19:24:36 +0100

    depurando

--- app/imgGen.py
+++ app/imgGen.py
@@ -7,7 +7,10 @@ from PIL import Image
 
 print("\n\t incio imgGen: importacin de librerias correcta")
 
-def generate_image_with_model(description, scene):
+def generate_image_with_model(scene):
+    
+    print("\n\t incio imgGen: importacin de librerias correcta")
+
     """
     Usa un modelo de Stable Diffusion para generar una imagen basada en la descripción.
     """
@@ -15,9 +18,8 @@ def generate_image_with_model(description, scene):
     pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to("cuda")  # Usa GPU si está disponible
 
-    prompt = f"{description}, {scene}"
-    image = pipe(prompt).images[0]
-
+    prompt = f"{scene}"
+    image = pipe(prompt, num_inference_steps=num_inference_steps, guidance_scale=guidance_scale, generator=generator).images[0]
 
     # Definir el nombre del archivo
     output_dir = "generated_images"
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ print("inocio routes")
 
 from flask import Blueprint, request, jsonify, current_app
 from .models import save_profile, generate_image
+from .imgGen import generate_image_with_model
 from flask import send_from_directory
 from flask import render_template
 
@@ -28,7 +29,7 @@ def generate_image_route():
 def favicon():
     return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@main.route('/generate-image', methods=['POST'])
+@main_bp.route('/generate_image_with_model', methods=['POST'])
 def generate_image_with_model_route():
     """
     Ruta para generar una imagen con Stable Diffusion.
@@ -36,15 +37,19 @@ def generate_image_with_model_route():
     try:
         # Obtener datos del cuerpo de la solicitud
         data = request.get_json()
+        print("data: ", data)
         description = data.get("description")
+        print("descripcion: ", description)
         scene = data.get("scene")
+        print("escena: ", scene)
 
-        if not description or not scene:
+        if not description: #or not scene:
             return jsonify({"error": "Faltan 'description' o 'scene' en la solicitud"}), 400
-
+        
+        print("antes de llamar a la función de generación de imágenes")
         # Llamar a la función de generación de imágenes
-        image_path = generate_image_with_model(description, scene)
-
+        image_path = generate_image_with_model(scene)
+        print("path imagen generada: ", image_path)
         # Devolver la ruta del archivo generado
         return jsonify({"message": "Imagen generada con éxito", "image_path": image_path})
 
